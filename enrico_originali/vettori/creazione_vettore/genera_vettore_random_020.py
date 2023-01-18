7eb6ce2236b11c54305058e255897196addd06b5
Alessio7adf <96028053+Alessio7adf@users.noreply.github.com>
2023-01-18 14:08:33 +0100

    uso del list comprehension

--- enrico_originali/vettori/creazione_vettore/genera_vettore_random_020.py
+++ enrico_originali/vettori/creazione_vettore/genera_vettore_random_020.py
@@ -1,15 +1,8 @@
-
 from random import random
 
 def genera_vettore_interi(nr_elementi = 10):
     """ genera un vettore di interi con valori casuali"""
-    vettore = []
-    for i in range(nr_elementi):
-        valore = random()*100
-        valore = int(valore)
-        print("genero {}-esimo elemento, e' {}".format(i, valore))
-        vettore.append(valore)
-
+    vettore = [int(random()*100) for i in range(nr_elementi)]
     return vettore
 
 vettore = genera_vettore_interi()
